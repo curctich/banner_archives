87485bcb7fa69afeb8993ce8d0ded47e5cb8359e
saori <sieeya.bp@gmail.com>
2021-08-15 02:09:35 +0900

    Layout.jsを作成

---
+++ components/Layout.js
@@ -0,0 +1,42 @@
+import Head from "next/head"
+import Link from "next/link"
+import Image from 'next/image'
+import styles from  '../styles/Home.module.scss'
+
+const Layout = (props) => {
+  const { title, children } = props
+  const siteTitle = "Banner Archives"
+  
+  return (
+      <div className="page">
+        <Head>
+          <title>{title ? `${title} | ${siteTitle}` : siteTitle}</title>
+          <link rel="icon" href="favicon.ico" />
+        </Head>
+        
+        <header>
+          <h1 className={styles.site_title}>
+            <Link href="/">
+              <a>{siteTitle}</a>
+            </Link>
+          </h1>
+        </header>
+        
+        <main>
+          {title ? <h1 className="page-title">{title}</h1> : ``}
+          <div className="page-main">
+            {children}
+          </div>
+        </main>
+        
+        <footer>
+          &copy; {siteTitle}
+          <span className="logo">
+             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </footer>
+      </div>
+  )
+}
+
+export default Layout
