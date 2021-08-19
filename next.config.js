2a5ef7c343df3a1ac855251785a70794594e13c8
saori <sieeya.bp@gmail.com>
2021-08-20 00:00:50 +0900

    Font Optimizationを有効化

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    optimizeFonts: true,
+  },
   sassOptions: {
     includePaths: [
         path.join(__dirname, 'styles')
